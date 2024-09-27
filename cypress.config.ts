06f9d22dc4c69f938064185c0ccf7d11b1f8bfce
Victor Doros <dorosvictor@gmail.com>
2024-09-27 08:13:18 +0300

    install cypress-mochawesome-reporter plugin

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "custom-title",
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   env: {
     grepFilterSpecs: true,
     grepOmitFiltered: true,
@@ -8,7 +19,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "https://simple-grocery-store-api-testing.glitch.me",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on)
       require("@cypress/grep/src/plugin")(config)
       return config
     },

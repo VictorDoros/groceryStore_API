5dbee0ee9687798633986b158be77fc864ee6405
Victor Doros <dorosvictor@gmail.com>
2025-01-09 07:33:43 +0200

    run on multiple environments changes

--- cypress/support/environment.ts
+++ cypress/support/environment.ts
@@ -2,9 +2,15 @@ export default class Environment {
   getEnvironment() {
     let env = Cypress.env("env")
     if (env === "testing") {
-      return "https://simple-grocery-store-api-testing.glitch.me"
+      return Cypress.config(
+        "baseUrl",
+        "https://simple-grocery-store-api-testing.glitch.me"
+      )
     } else if (env === "prod") {
-      return "https://simple-grocery-store-api.glitch.me"
+      return Cypress.config(
+        "baseUrl",
+        "https://simple-grocery-store-api.glitch.me"
+      )
     }
   }
 }
